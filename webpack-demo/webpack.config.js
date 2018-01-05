cb7be024ab6f227970d667d7e60f8ecef7307c4c
yuanruixiangj <yuanruixiangjet@163.com>
2018-01-05 19:01:26 +0800

    0105 1st

---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path')
+var htmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: {
+        main: './src/script/main.js',
+        a: './src/script/a.js',
+        b: './src/script/b.js',
+        c: './src/script/c.js',
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'js/[name]-[chunkhash].js',
+        publicPath:'http://cdn.com/'
+    },
+    plugins:[
+        new htmlWebpackPlugin({
+            filename:'a.html',
+            template: 'index.html',
+            inject: false,
+            title: 'this is a.html',
+            excludeChunks: ['b','c']
+        }),
+
+        new htmlWebpackPlugin({
+            filename:'b.html',
+            template: 'index.html',
+            inject: false,
+            title: 'this is b.html',
+            excludeChunks: ['a','c']
+        }),
+
+        new htmlWebpackPlugin({
+            filename:'c.html',
+            template: 'index.html',
+            inject: false,
+            title: 'this is c.html',
+            excludeChunks: ['a','b']
+        })
+    ]
+}
